2a2d39a56dc66d23f086316679e6125723f999f0
yullie lima <yullielimas@gmail.com>
2022-03-02 15:28:32 -0800

    final sec5

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -4,7 +4,7 @@ import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
   if (props.items.length === 0) {
-    return <h2 className="expenses-list__fallback">Found no expenses</h2>;
+    return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
   }
 
   return (
